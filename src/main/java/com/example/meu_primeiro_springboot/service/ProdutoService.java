330fdf54546f13b4302568898bf45fc540de1dd3
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-18 17:26:19 -0300

    Completa CRUD

--- src/main/java/com/example/meu_primeiro_springboot/service/ProdutoService.java
+++ src/main/java/com/example/meu_primeiro_springboot/service/ProdutoService.java
@@ -1,11 +1,12 @@
 package com.example.meu_primeiro_springboot.service;
 
-import com.example.meu_primeiro_springboot.model.Produto;
-import com.example.meu_primeiro_springboot.repository.ProdutoRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
-import java.awt.color.ProfileDataException;
-import java.util.List;
+import com.example.meu_primeiro_springboot.model.Produto;
+import com.example.meu_primeiro_springboot.repository.ProdutoRepository;
 
 @Service
 public class ProdutoService {
@@ -16,4 +17,15 @@ public class ProdutoService {
     public List<Produto> listarProdutos(){
         return produtoRepository.findAll();
     }
+    public Optional<Produto> buscarPorId(Long id){
+        return produtoRepository.findById(id);
+    }
+    public Produto salvarProduto(Produto produto){
+        return  produtoRepository.save(produto);
+    }
+    public void deletarProduto(Long id){
+        produtoRepository.deleteById(id);
+    }
+
+
 }
